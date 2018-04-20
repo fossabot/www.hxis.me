d27c04bb80ab6ac8cebadc7887b5b953800b0dc2
惶心 <30528433+hxcloud@users.noreply.github.com>
2018-04-20 21:41:41 +0100

    Update background.js

--- statics/js/background.js
+++ statics/js/background.js
@@ -2,7 +2,6 @@ var quotes = new Array();
 quotes[0] = "https://api.i-meto.com/bing?new";
 quotes[1] = "https://api.ikmoe.com/moeu-api.php";
 quotes[2] = "https://api.paugram.com/wallpaper/?source=sina";
-quotes[4] = "https://api.i-meto.com/bing"
 
 var q = quotes.length;
 var whichquote = Math.round(Math.random() * (q - 1));
