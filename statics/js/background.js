8d5ad7c2b7ebce43dce0da1572c618c12e02dd60
惶心 <30528433+hxcloud@users.noreply.github.com>
2018-04-20 21:34:39 +0100

    Add files via upload

---
+++ statics/js/background.js
@@ -0,0 +1,16 @@
+var quotes = new Array();
+quotes[0] = "https://api.i-meto.com/bing?new";
+quotes[1] = "https://api.ikmoe.com/moeu-api.php";
+quotes[2] = "https://api.paugram.com/wallpaper/?source=sina";
+quotes[4] = "https://api.i-meto.com/bing"
+
+var q = quotes.length;
+var whichquote = Math.round(Math.random() * (q - 1));
+
+function showquote() {
+    document.write('<style>' + '@media only screen and (min-width:768px){body{background-image:url(' + quotes[
+            whichquote] + ')}}' + '@media only screen and (max-width:768px){body{background-image:url(' +
+        quotes[whichquote] + ')}}' + '</style>');
+}
+
+showquote();
---
+++ statics/js/redirect.js
@@ -0,0 +1,3 @@
+    if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+        window.location.href = "https://tech.hxco.de";
+    };
